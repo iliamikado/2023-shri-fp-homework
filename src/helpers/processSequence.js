55a09e4e3c18bf3a1e0a7722bb70c66501219dfa
iliamikado <iliamikado@gmail.com>
2023-07-23 16:41:13 +0300

    HW done

--- src/helpers/processSequence.js
+++ src/helpers/processSequence.js
@@ -14,38 +14,56 @@
  * Иногда промисы от API будут приходить в состояние rejected, (прямо как и API в реальной жизни)
  * Ответ будет приходить в поле {result}
  */
- import Api from '../tools/api';
 
- const api = new Api();
+import Api from '../tools/api';
 
- /**
-  * Я – пример, удали меня
-  */
- const wait = time => new Promise(resolve => {
-     setTimeout(resolve, time);
- })
+const api = new Api();
 
- const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
-     /**
-      * Я – пример, удали меня
-      */
-     writeLog(value);
+const addWriteLog = (writeLog) => ((fn) => (async (...args) => {
+    let ans = await fn(...args);
+    if (typeof ans === 'object') {
+        ans = ans.result;
+    }
+    writeLog(ans);
+    return ans;
+}));
+const validate = (str) => {
+    const n = +str;
+    if (isNaN(n)) {
+        throw new Error('ValidationError')
+    }
+    return n;
+};
+const round = Math.round;
+const getLength = (n) => (String(n).length);
+const square = (n) => (n * n);
+const mod3 = (n) => (n % 3);
 
-     api.get('https://api.tech/numbers/base', {from: 2, to: 10, number: '01011010101'}).then(({result}) => {
-         writeLog(result);
-     });
 
-     wait(2500).then(() => {
-         writeLog('SecondLog')
+const processSequence = ({value, writeLog, handleSuccess, handleError}) => {
+    writeLog(value);
 
-         return wait(1500);
-     }).then(() => {
-         writeLog('ThirdLog');
+    const [roundLog, apiGetBaseLog, getLengthLog, squareLog, mod3Log] = [
+        round,
+        api.get('https://api.tech/numbers/base'),
+        getLength,
+        square,
+        mod3
+    ].map(addWriteLog(writeLog));
 
-         return wait(400);
-     }).then(() => {
-         handleSuccess('Done');
-     });
- }
+    Promise.resolve(value)
+    .then(validate)
+    .then(roundLog)
+    .then((data) => (apiGetBaseLog({from: 10, to: 2, number: data})))
+    .then(getLengthLog)
+    .then(squareLog)
+    .then(mod3Log)
+    .then((id) => (api.get(`https://animals.tech/${id}`)({})))
+    .then(({result}) => handleSuccess(result))
+    .catch(e => {
+        console.log(e);
+        handleError(e.message);
+    });
+}
 
 export default processSequence;
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,3 +1,4 @@
+import { allPass, pipe, not, equals } from "ramda";
 /**
  * @file Домашка по FP ч. 1
  *
@@ -13,38 +14,85 @@
  * Если какие либо функции написаны руками (без использования библиотек) это не является ошибкой
  */
 
-// 1. Красная звезда, зеленый квадрат, все остальные белые.
-export const validateFieldN1 = ({star, square, triangle, circle}) => {
-    if (triangle !== 'white' || circle !== 'white') {
-        return false;
-    }
+const isWhite = (figure) => (figure === 'white');
+const isRed = (figure) => (figure === 'red');
+const isGreen = (figure) => (figure === 'green');
+const isOrange = (figure) => (figure === 'orange');
+const isBlue = (figure) => (figure === 'blue');
 
-    return star === 'red' && square === 'green';
-};
+const countColors = (props) => (
+    Object.getOwnPropertyNames(props).reduce((prev, figure) => {
+        prev[props[figure]] = (prev[props[figure]] || 0) + 1;
+        return prev;
+    }, {})
+);
+const countColor = (color) => (pipe(countColors, (props) => (props[color] || 0))); 
+const moreThan = (n) => ((x) => (x > n));
+const equalAnswers = (fn1, fn2) => ((props) => (fn1(props) === fn2(props)));
+
+const getTriangle = ({triangle}) => (triangle);
+const getSquare = ({square}) => (square);
+
+const isStarRed = ({star}) => (isRed(star));
+const isStarWhite = ({star}) => (isWhite(star));
+const isSquareGreen = ({square}) => (isGreen(square));
+const isTriangleWhite = ({triangle}) => (isWhite(triangle));
+const isCircleWhite = ({circle}) => (isWhite(circle));
+const isCircleBlue = ({circle}) => (isBlue(circle));
+const isSquareOrange = ({square}) => (isOrange(square));
+const isTriangleGreen = ({triangle}) => (isGreen(triangle));
+
+// 1. Красная звезда, зеленый квадрат, все остальные белые.
+export const validateFieldN1 = allPass([
+    isStarRed,
+    isSquareGreen,
+    isTriangleWhite,
+    isCircleWhite
+])
 
 // 2. Как минимум две фигуры зеленые.
-export const validateFieldN2 = () => false;
+export const validateFieldN2 = pipe(countColor('green'), moreThan(1));
 
 // 3. Количество красных фигур равно кол-ву синих.
-export const validateFieldN3 = () => false;
+export const validateFieldN3 = equalAnswers(countColor('red'), countColor('blue'));
 
 // 4. Синий круг, красная звезда, оранжевый квадрат треугольник любого цвета
-export const validateFieldN4 = () => false;
+export const validateFieldN4 = allPass([
+    isCircleBlue,
+    isStarRed,
+    isSquareOrange
+]);
 
 // 5. Три фигуры одного любого цвета кроме белого (четыре фигуры одного цвета – это тоже true).
-export const validateFieldN5 = () => false;
+export const validateFieldN5 = pipe(
+    countColors,
+    (colors) => (Object.getOwnPropertyNames(colors)
+        .map((color) => (color === 'white' ? 0 :    colors[color]))),
+    (arr) => (Math.max(...arr)),
+    moreThan(2)
+)
 
 // 6. Ровно две зеленые фигуры (одна из зелёных – это треугольник), плюс одна красная. Четвёртая оставшаяся любого доступного цвета, но не нарушающая первые два условия
-export const validateFieldN6 = () => false;
+export const validateFieldN6 = allPass([
+    isTriangleGreen,
+    pipe(countColor('green'), equals(2)),
+    pipe(countColor('red'), equals(1))
+]);
 
 // 7. Все фигуры оранжевые.
-export const validateFieldN7 = () => false;
+export const validateFieldN7 = pipe(countColor('orange'), equals(4));
 
 // 8. Не красная и не белая звезда, остальные – любого цвета.
-export const validateFieldN8 = () => false;
+export const validateFieldN8 = allPass([
+    pipe(isStarRed, not),
+    pipe(isStarWhite, not)
+]);
 
 // 9. Все фигуры зеленые.
-export const validateFieldN9 = () => false;
+export const validateFieldN9 = pipe(countColor('green'), equals(4));
 
 // 10. Треугольник и квадрат одного цвета (не белого), остальные – любого цвета
-export const validateFieldN10 = () => false;
+export const validateFieldN10 = allPass([
+    pipe(isTriangleWhite, not),
+    equalAnswers(getSquare, getTriangle)
+]);
